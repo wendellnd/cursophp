813fbf9e6d15be422429646927b1177556a70448
wendellnd <wendelldourado.wn@gmail.com>
2023-08-08 22:15:42 -0300

    array_push.php

---
+++ variables/array_push.php
@@ -0,0 +1,17 @@
+<?php
+
+$people = array();
+
+array_push($people, array(
+    'name'=>'João',
+    'age'=>20
+));
+
+array_push($people, array(
+    'name'=>'Wends',
+    'age'=>20
+));
+
+print_r($people[0]['name']);
+
+?>
